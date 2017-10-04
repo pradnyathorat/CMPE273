db8bc5d2eb72fa3e913426b5131a20b8c944f638
Pradnya Thorat <pradnya_11492@yahoo.co.in>
2017-10-03 19:02:18 -0700

    Calculator Implementation

--- routes/mainPage.js
+++ routes/mainPage.js
@@ -13,9 +13,73 @@ exports.index = function(req,res) {
    });
 }
 
+
 exports.findSolution = function(req,res){
+	var operation = req.param("op");
+	var input1 = req.param("input1");
+	var input2 = req.param("input2");
+	var result = req.param("result");
 
+	if(input1=="" || input2==""){
+		ejs.renderFile('./views/index.ejs', { invalid: 1 } , function(err, results) {
+	        if (!err) {
+	            res.end(results);
+	        }
+	        else {
+	            res.end('An error occurred');
+	            console.log(err);
+	        }
+	    });
+	}
+	else
+		{
+		if (operation == "add") {
+				result=parseFloat(input1)+parseFloat(input2);
+		}
+		if (operation == "subtract") {
+			result=parseFloat(input1)-parseFloat(input2);
+		}
+		if (operation == "multiply") {
+			result=parseFloat(input1)*parseFloat(input2);
+		}
+		if (operation == "divide") {
+			if(parseFloat(input2) && parseInt(input2)!=0)
+			{
+				result=parseFloat(input1)/parseFloat(input2);
+			}
+			else
+			{
+				ejs.renderFile('./views/index.ejs', { invalid: 2 } , function(err, results) {
+			        if (!err) {
+			            res.end(results);
+			        }
+			        else {
+			            res.end('An error occurred');
+			            console.log(err);
+			        }
+			    });
+			}
+		}
+		if(operation=='clear'){
+			ejs.renderFile('./views/index.ejs', function(err, results) {
+		        if (!err) {
+		            res.end(results);
+		        }
+		        else {
+		            res.end('An error occurred');
+		            console.log(err);
+		        }
+		    });
+		}
+		console.log("Result --> "+result);
+		ejs.renderFile('./views/index.ejs', { result: result, input1 : input1, input2 : input2} , function(err, results) {
+	        if (!err) {
+	            res.end(results);
+	        }
+	        else {
+	            res.end('An error occurred');
+	            console.log(err);
+	        }
+	    });
+		}
 };
-
-
-
